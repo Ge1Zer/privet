d7abd0a8d354e4fa49a0b7dc7c55f242d07bd4f6
ProhodaKi <geizer@ro.ru>
2020-10-08 13:44:14 +0400

    Profile start setting add place ava name button skype email

--- src/components/layout/Menu/view/ProfileView/index.tsx
+++ src/components/layout/Menu/view/ProfileView/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './styles/styles.scss'
 const ProfileView : React.FC = () =>{
     return (
         <>
-            <p>ProfileCase</p>
+            <img src="#" alt="ava"/>
+            <p>Name</p>
+            <nav>
+                <ul>
+                    <li>I</li>
+                    <li>I</li>
+                </ul>
+            </nav>
+            <button>Message</button>
+            <p>skype</p>
+            <p>email</p>
         </>
     )
 }
